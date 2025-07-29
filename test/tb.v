1501d27ec2084f887bf23ee0cad49842947c6dba
SuvarsithaVallamsetti <suvarsithavallamsetti@gmail.com>
2025-07-29 12:11:22 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,14 +1,28 @@
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
 
-module tb_restoring_divider;
+module tb;
 
-    reg [7:0] in;
-    wire [7:0] out;
+    reg  [7:0] ui_in;
+    wire [7:0] uo_out;
 
-    // Instantiate the User Module Under Test (UUT)
-    tt_um_restoring_divider uut (
-        .in(in),
-        .out(out)
+    // Unused IOs
+    wire [7:0] uio_out;
+    wire [7:0] uio_oe;
+    reg  [7:0] uio_in = 8'b0;
+    reg clk = 1'b0;
+    reg rst_n = 1'b1;
+    reg ena = 1'b1;
+
+    // Instantiate the divider module
+    tt_um_unsigned_divider uut (
+        .ui_in(ui_in),
+        .uo_out(uo_out),
+        .uio_in(uio_in),
+        .uio_out(uio_out),
+        .uio_oe(uio_oe),
+        .clk(clk),
+        .rst_n(rst_n),
+        .ena(ena)
     );
 
     initial begin
@@ -16,29 +30,29 @@ module tb_restoring_divider;
         $display("------------------------------------------------");
 
         // Test Case 1: 10 / 3
-        in = {4'd10, 4'd3};
+        ui_in = {4'd10, 4'd3};
         #10;
-        $display("%4t |    %2d    |   %2d    |    %2d    |     %2d", $time, in[7:4], in[3:0], out[7:4], out[3:0]);
+        $display("%4t |    %2d    |   %2d    |    %2d    |     %2d", $time, ui_in[7:4], ui_in[3:0], uo_out[7:4], uo_out[3:0]);
 
         // Test Case 2: 15 / 5
-        in = {4'd15, 4'd5};
+        ui_in = {4'd15, 4'd5};
         #10;
-        $display("%4t |    %2d    |   %2d    |    %2d    |     %2d", $time, in[7:4], in[3:0], out[7:4], out[3:0]);
+        $display("%4t |    %2d    |   %2d    |    %2d    |     %2d", $time, ui_in[7:4], ui_in[3:0], uo_out[7:4], uo_out[3:0]);
 
         // Test Case 3: 9 / 2
-        in = {4'd9, 4'd2};
+        ui_in = {4'd9, 4'd2};
         #10;
-        $display("%4t |    %2d    |   %2d    |    %2d    |     %2d", $time, in[7:4], in[3:0], out[7:4], out[3:0]);
+        $display("%4t |    %2d    |   %2d    |    %2d    |     %2d", $time, ui_in[7:4], ui_in[3:0], uo_out[7:4], uo_out[3:0]);
 
         // Test Case 4: 7 / 3
-        in = {4'd7, 4'd3};
+        ui_in = {4'd7, 4'd3};
         #10;
-        $display("%4t |    %2d    |   %2d    |    %2d    |     %2d", $time, in[7:4], in[3:0], out[7:4], out[3:0]);
+        $display("%4t |    %2d    |   %2d    |    %2d    |     %2d", $time, ui_in[7:4], ui_in[3:0], uo_out[7:4], uo_out[3:0]);
 
         // Test Case 5: 8 / 4
-        in = {4'd8, 4'd4};
+        ui_in = {4'd8, 4'd4};
         #10;
-        $display("%4t |    %2d    |   %2d    |    %2d    |     %2d", $time, in[7:4], in[3:0], out[7:4], out[3:0]);
+        $display("%4t |    %2d    |   %2d    |    %2d    |     %2d", $time, ui_in[7:4], ui_in[3:0], uo_out[7:4], uo_out[3:0]);
 
         $finish;
     end
